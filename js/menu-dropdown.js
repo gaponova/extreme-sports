22f374a16b5684f8e8ca8d2324c45de5159d0102
gaponova <dzensmart@gmail.com>
2022-07-07 19:55:44 +0300

    Пофиксили меню и стрелку

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -27,23 +27,23 @@ let isMobile = {
 };
 
 let body = document.querySelector("body");
-//if (isMobile.any()) {
-body.classList.add("touch");
-let arrow = document.querySelectorAll(".arrow");
-for (i = 0; i < arrow.length; i++) {
-  let thisLink = arrow[i].previousElementSibling;
-  let subMenu = arrow[i].nextElementSibling;
-  let thisArrow = arrow[i];
+if (isMobile.any()) {
+  body.classList.add("touch");
+  let arrow = document.querySelectorAll(".arrow");
+  for (i = 0; i < arrow.length; i++) {
+    let thisLink = arrow[i].previousElementSibling;
+    let subMenu = arrow[i].nextElementSibling;
+    let thisArrow = arrow[i];
 
-  thisLink.classList.add("parent");
-  arrow[i].addEventListener("click", function () {
-    subMenu.classList.toggle("open");
-    thisArrow.classList.toggle("active");
-  });
+    thisLink.classList.add("parent");
+    arrow[i].addEventListener("click", function () {
+      subMenu.classList.toggle("open");
+      thisArrow.classList.toggle("active");
+    });
+  }
+} else {
+  body.classList.add("mouse");
 }
-//} else {
-//body.classList.add("mouse");
-//}
 
 //Бургер
 $(document).ready(function () {
